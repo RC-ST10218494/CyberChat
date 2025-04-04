a85fb99ae6403da8f422420d26c725ce35a74278
RC-ST10218494 <st10218494@rcconnect.edu.za>
2025-04-04 16:53:16 +0200

    Fix: Added Main method and corrected project setup

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Media;
 
 class Program
 {
     static void Main()
     {
         Console.WriteLine("Welcome to CyberChat!");
+
+        // Keep console open
+        Console.ReadLine();
     }
 }
